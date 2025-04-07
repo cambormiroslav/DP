b4501c23a93be3f5d3a5de1c3c78e4ec6f21227e
Miroslav <mira.cambor@email.cz>
2025-04-07 17:17:31 +0200

    prevent code duplication

--- projekt/gemini.py
+++ projekt/gemini.py
@@ -6,8 +6,12 @@ import time
 import functions
 
 api_key = os.environ["GEMINI_API_KEY"]
-pattern = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
-correct_data_path = "../data_for_control/dataset_correct_data.json"
+
+ocr_method = True
+
+if ocr_method:
+    pattern = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
+    correct_data_path = "../data_for_control/dataset_correct_data.json"
 
 model_text = "gemini-1.5-flash"
 model_is_pro = False
@@ -38,7 +42,7 @@ def send_image_request(image_path, text_request):
 Input: (Path to directory with input images, count of input images)
 Output: None (but call save to file)
 """
-def load_and_measure(dir_path, first_ticket, latest_ticket):
+def load_and_measure(dir_path, first_ticket, latest_file):
     i = first_ticket - 1
     array_of_images = os.listdir(dir_path)
     while(True):
@@ -47,36 +51,46 @@ def load_and_measure(dir_path, first_ticket, latest_ticket):
         response = send_image_request(dir_path + file, pattern)
         end_datetime = datetime.datetime.now()
 
-        data_tuple = functions.check_the_data_ocr(response, file, correct_data_path)
-        correctness = data_tuple[0]
-        correct_data = data_tuple[1]
-        incorect_data = data_tuple[2]
-        not_found_data = data_tuple[3]
-        good_not_found = data_tuple[4]
-        dict_of_incorect = data_tuple[5]
-        array_not_found = data_tuple[6]
-        array_good_not_found = data_tuple[7]
+        if ocr_method:
+            data_tuple = functions.check_the_data_ocr(response, file, correct_data_path)
+            correctness = data_tuple[0]
+            correct_data = data_tuple[1]
+            incorect_data = data_tuple[2]
+            not_found_data = data_tuple[3]
+            good_not_found = data_tuple[4]
+            dict_of_incorect = data_tuple[5]
+            array_not_found = data_tuple[6]
+            array_good_not_found = data_tuple[7]
+        
         diff_datetime = end_datetime - start_datetime
         diff_datetime_seconds = diff_datetime.total_seconds()
 
-        functions.save_to_file_ocr(model_text, "ticket", [correctness, correct_data, 
-                                                 incorect_data, not_found_data, 
-                                                 good_not_found, diff_datetime_seconds], 
-                                                 dict_of_incorect, array_not_found, 
-                                                 array_good_not_found)
-        print(correctness, correct_data, incorect_data, not_found_data, 
-              good_not_found, diff_datetime_seconds, dict_of_incorect,
-              array_not_found, array_good_not_found)
+        if ocr_method:
+            functions.save_to_file_ocr(model_text, "ticket", [correctness, correct_data, 
+                                                              incorect_data, not_found_data, 
+                                                              good_not_found, diff_datetime_seconds], 
+                                                              dict_of_incorect, array_not_found, 
+                                                              array_good_not_found)
+        
+        if ocr_method:
+            print(correctness, correct_data, incorect_data, not_found_data, 
+                  good_not_found, diff_datetime_seconds, dict_of_incorect,
+                  array_not_found, array_good_not_found)
+            
         i += 1
-        print("Receipt: ", i)
 
-        if i == latest_ticket:
+        if ocr_method:
+            print("Receipt: ", i)
+
+        if i == latest_file:
             break
+
         if model_is_pro:
             time.sleep(15.0)
 
 if __name__ == "__main__":
-    dir_path = "../dataset/large-receipt-image-dataset-SRD/"
+    if ocr_method:
+        dir_path = "../dataset/large-receipt-image-dataset-SRD/"
 
     load_and_measure(dir_path, 1, 103)
 
--- projekt/ollama_api.py
+++ projekt/ollama_api.py
@@ -6,8 +6,11 @@ import datetime
 
 import functions
 
-pattern = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, fax number in key fax_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
-correct_data_path = "../data_for_control/dataset_correct_data.json"
+ocr_method = True
+
+if ocr_method:
+    pattern = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, fax number in key fax_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
+    correct_data_path = "../data_for_control/dataset_correct_data.json"
 
 model = "llava"
 #model = "bakllava"
@@ -56,7 +59,7 @@ def send_image_request(image_base64, text_request):
 Input: (Path to directory with input images, count of input images)
 Output: None (but call save to file)
 """
-def load_and_measure(dir_path, first_ticket, latest_ticket):
+def load_and_measure(dir_path, first_ticket, latest_file):
     i = first_ticket - 1
     array_of_images = os.listdir(dir_path)
     while(True):
@@ -66,53 +69,64 @@ def load_and_measure(dir_path, first_ticket, latest_ticket):
         response = send_image_request(base_64_image, pattern)
         end_datetime = datetime.datetime.now()
 
-        data_tuple = functions.check_the_data_ocr(response, file, correct_data_path)
-        correctness = data_tuple[0]
-        correct_data = data_tuple[1]
-        incorect_data = data_tuple[2]
-        not_found_data = data_tuple[3]
-        good_not_found = data_tuple[4]
-        dict_of_incorect = data_tuple[5]
-        array_not_found = data_tuple[6]
-        array_good_not_found = data_tuple[7]
+        if ocr_method:
+            data_tuple = functions.check_the_data_ocr(response, file, correct_data_path)
+            correctness = data_tuple[0]
+            correct_data = data_tuple[1]
+            incorect_data = data_tuple[2]
+            not_found_data = data_tuple[3]
+            good_not_found = data_tuple[4]
+            dict_of_incorect = data_tuple[5]
+            array_not_found = data_tuple[6]
+            array_good_not_found = data_tuple[7]
+        
         diff_datetime = end_datetime - start_datetime
         diff_datetime_seconds = diff_datetime.total_seconds()
 
         if model == "knoopx/mobile-vlm:3b-fp16":
-            functions.save_to_file_ocr("knoopx-mobile-vlm-3b-fp16", "ticket", [correctness, correct_data, 
-                                                 incorect_data, not_found_data, 
-                                                 good_not_found, diff_datetime_seconds], 
-                                                 dict_of_incorect, array_not_found, 
-                                                 array_good_not_found)
+            if ocr_method:
+                functions.save_to_file_ocr("knoopx-mobile-vlm-3b-fp16", "ticket", [correctness, correct_data, 
+                                                                                   incorect_data, not_found_data, 
+                                                                                   good_not_found, diff_datetime_seconds], 
+                                                                                   dict_of_incorect, array_not_found, 
+                                                                                   array_good_not_found)
         elif model == "llava:13b":
-            functions.save_to_file_ocr("llava-13b", "ticket", [correctness, correct_data, 
-                                                 incorect_data, not_found_data, 
-                                                 good_not_found, diff_datetime_seconds], 
-                                                 dict_of_incorect, array_not_found, 
-                                                 array_good_not_found)
+            if ocr_method:
+                functions.save_to_file_ocr("llava-13b", "ticket", [correctness, correct_data, 
+                                                                   incorect_data, not_found_data, 
+                                                                   good_not_found, diff_datetime_seconds], 
+                                                                   dict_of_incorect, array_not_found, 
+                                                                   array_good_not_found)
         elif model == "llava:34b":
-            functions.save_to_file_ocr("llava-34b", "ticket", [correctness, correct_data, 
-                                                 incorect_data, not_found_data, 
-                                                 good_not_found, diff_datetime_seconds], 
-                                                 dict_of_incorect, array_not_found, 
-                                                 array_good_not_found)
+            if ocr_method:
+                functions.save_to_file_ocr("llava-34b", "ticket", [correctness, correct_data, 
+                                                                   incorect_data, not_found_data, 
+                                                                   good_not_found, diff_datetime_seconds], 
+                                                                   dict_of_incorect, array_not_found, 
+                                                                   array_good_not_found)
         else:
-            functions.save_to_file_ocr(model, "ticket", [correctness, correct_data, 
-                                                 incorect_data, not_found_data, 
-                                                 good_not_found, diff_datetime_seconds], 
-                                                 dict_of_incorect, array_not_found, 
-                                                 array_good_not_found)
-        print(correctness, correct_data, incorect_data, not_found_data, 
-              good_not_found, diff_datetime_seconds, dict_of_incorect,
-              array_not_found, array_good_not_found)
+            if ocr_method:
+                functions.save_to_file_ocr(model, "ticket", [correctness, correct_data, 
+                                                             incorect_data, not_found_data, 
+                                                             good_not_found, diff_datetime_seconds], 
+                                                             dict_of_incorect, array_not_found, 
+                                                         array_good_not_found)
+        if ocr_method:
+            print(correctness, correct_data, incorect_data, not_found_data, 
+                  good_not_found, diff_datetime_seconds, dict_of_incorect,
+                  array_not_found, array_good_not_found)
+        
         i += 1
-        print("Receipt: ", i)
 
-        if i == latest_ticket:
+        if ocr_method:
+            print("Receipt: ", i)
+
+        if i == latest_file:
             break
     
 if __name__ == "__main__":
-    dir_path = "../dataset/large-receipt-image-dataset-SRD/"
+    if ocr_method:
+        dir_path = "../dataset/large-receipt-image-dataset-SRD/"
 
     load_and_measure(dir_path, 1, 103)
     #print(send_image_request(get_image_in_base64(dir_path + "1000-receipt.jpg"), pattern))
