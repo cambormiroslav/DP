41ac24f7a067bf41736eee46298041b0061d0511
cambormiroslav <miroslav.cambor@tul.cz>
2026-02-07 22:01:38 +0100

    fix code duplication

--- projekt/test_patterns.py
+++ projekt/test_patterns.py
@@ -15,47 +15,27 @@ if delete_dirs:
     if os.path.exists(functions.pattern_test_object_dir_output_path):
         shutil.rmtree(functions.pattern_test_object_dir_output_path)
 
-pattern1G_OcrEn = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find the fax number show it too as fax_number. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
-pattern1G_OcrCz = "Zjisti mi ze snímku seznam zboží. Vrať mi adresu, datum, čas a název společnosti. Když najdeš telefonní číslo, vrať ho také. Když najdeš faxové číslo, vrať ho také jako fax_number. Když najdeš číslo stolu, informace o počtu hostů nebo číslo objednávky, vrať je také. Výstup mi ukaž jako JSON. Název společnosti navrať pod klíčem company, adresu společnosti pod klíčem address, telefonní číslo pod klíčem phone_number, jméno číšníka pod klíčem server, číslo stanice pod klíčem station, číslo objednávky pod klíčem order_number, informace o stole pod klíčem table, počet hostů pod klíčem guests, mezisoučet ceny pod klíčem sub_total, daň pod klíčem tax, celkovou cenu pod klíčem total, datum pod klíčem date, čas pod klíčem time. Každý název zboží bude jako klíč JSON v klíči goods a hodnota zboží bude další JSON s množstvím zboží v klíči amount a cenou zboží v klíči price."
-pattern1G_ObjectEn = "Detect all peaple. Every person is described by one JSON. Every person has the label person and with confidence score in key confidence."
-pattern1G_ObjectCz = "Detekuj všechny osoby. Každá osoba je popsána jedním JSONem. Každá osoba má štítek person a s hodnotou spolehlivosti v klíči confidence."
-
-pattern2Op_OcrEn = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
-pattern2Op_OcrCz = "Zjisti mi ze snímku seznam zboží. Vrať mi adresu, datum, čas a název společnosti. Když najdeš telefonní číslo, vrať ho také. Když najdeš číslo stolu, informace o počtu hostů nebo číslo objednávky, vrať je také. Výstup mi ukaž jako JSON. Název společnosti navrať pod klíčem company, adresu společnosti pod klíčem address, telefonní číslo pod klíčem phone_number, jméno číšníka pod klíčem server, číslo stanice pod klíčem station, číslo objednávky pod klíčem order_number, informace o stole pod klíčem table, počet hostů pod klíčem guests, mezisoučet ceny pod klíčem sub_total, daň pod klíčem tax, celkovou cenu pod klíčem total, datum pod klíčem date, čas pod klíčem time. Každý název zboží bude jako klíč JSON v klíči goods a hodnota zboží bude další JSON s množstvím zboží v klíči amount a cenou zboží v klíči price."
-pattern2Op_ObjectEn = "Detect all peaple. For every person add the string person to name key, x-min coordinate of person add to x_min key, x-max coordinate of person add to x_max key, y-min coordinate of person add to y_min key, y-max coordinate of person add to y_max key and confidence score in key confidence. All this data are given as JSON and added to JSON array. This JSON array add to key objects."
-pattern2Op_ObjectCz = "Detekuj všechny osoby. Pro každou osobu přidej řetězec person do klíče name, x-min souřadnici osoby přidej do klíče x_min, x-max souřadnici osoby přidej do klíče x_max, y-min souřadnici osoby přidej do klíče y_min, y-max souřadnici osoby přidej do klíče y_max a hodnotu spolehlivosti v klíči confidence. Všechny tyto údaje jsou uvedeny jako JSON a přidány do JSON pole. Právě vytvořené JSON pole přidej do výsledku s klíčem objects."
-
-pattern3Ol_OcrEn = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, fax number in key fax_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price. Return it as only JSON."
-pattern3Ol_OcrCz = "Zjisti mi ze snímku seznam zboží. Vrať mi adresu, datum, čas a název společnosti. Když najdeš telefonní číslo, vrať ho také. Když najdeš faxové číslo, vrať ho také jako fax_number. Když najdeš číslo stolu, informace o počtu hostů nebo číslo objednávky, vrať je také. Výstup mi ukaž jako JSON. Název společnosti navrať pod klíčem company, adresu společnosti pod klíčem address, telefonní číslo pod klíčem phone_number, faxové číslo pod klíčem fax_number, jméno číšníka pod klíčem server, číslo stanice pod klíčem station, číslo objednávky pod klíčem order_number, informace o stole pod klíčem table, počet hostů pod klíčem guests, mezisoučet ceny pod klíčem sub_total, daň pod klíčem tax, celkovou cenu pod klíčem total, datum pod klíčem date, čas pod klíčem time. Každý název zboží bude jako klíč JSON v klíči goods a hodnota zboží bude další JSON s množstvím zboží v klíči amount a cenou zboží v klíči price. Vrať to pouze jako JSON."
-pattern3Ol_ObjectEn = "Detect all peaple. For every person add the string person to name key, x-min coordinate of person add to x_min key, x-max coordinate of person add to x_max key, y-min coordinate of person add to y_min key, y-max coordinate of person add to y_max key and confidence score add to confidence key. All this data are given as JSON and added to JSON array. This JSON array add to key objects. Return only JSON output with detections."
-pattern3Ol_ObjectCz = "Detekuj všechny osoby. Pro každou osobu přidej řetězec person do klíče name, x-min souřadnici osoby přidej do klíče x_min, x-max souřadnici osoby přidej do klíče x_max, y-min souřadnici osoby přidej do klíče y_min, y-max souřadnici osoby přidej do klíče y_max a hodnotu spolehlivosti přidej do klíče confidence. Všechny tyto údaje jsou uvedeny jako JSON a přidány do JSON pole. Právě vytvořené JSON pole přidej do výsledku s klíčem objects. Vrať to pouze jako JSON výstup s detekcemi."
-
-pattern4Ol_OcrEn = "Get me the list of goods from picture. Show the address, date, time and name of company. When you find the phone number show it too. When you find you find the table number, the information about guest or order number show it too. Show me the output as JSON. The company name put in key company, the address of company in key address, phone number in key phone_number, fax number in key fax_number, server name in key server, station number in key station, order number in key order_number, table info in key table, number of guests in key guests, subtotal price to key sub_total, tax in key tax, total cost in key total, date in key date, time in key time. Every good name will be as key of the JSON in key goods and value of the good will be the another JSON with amount of goods in key amount and the cost of the good in key price."
-pattern4Ol_OcrCz = "Zjisti mi ze snímku seznam zboží. Vrať mi adresu, datum, čas a název společnosti. Když najdeš telefonní číslo, vrať ho také. Když najdeš faxové číslo, vrať ho také jako fax_number. Když najdeš číslo stolu, informace o počtu hostů nebo číslo objednávky, vrať je také. Výstup mi ukaž jako JSON. Název společnosti navrať pod klíčem company, adresu společnosti pod klíčem address, telefonní číslo pod klíčem phone_number, faxové číslo pod klíčem fax_number, jméno číšníka pod klíčem server, číslo stanice pod klíčem station, číslo objednávky pod klíčem order_number, informace o stole pod klíčem table, počet hostů pod klíčem guests, mezisoučet ceny pod klíčem sub_total, daň pod klíčem tax, celkovou cenu pod klíčem total, datum pod klíčem date, čas pod klíčem time. Každý název zboží bude jako klíč JSON v klíči goods a hodnota zboží bude další JSON s množstvím zboží v klíči amount a cenou zboží v klíči price."
-#pattern4Ol_ObjectEn - stejné jako u pattern2Op_ObjectEn
-#pattern4Ol_ObjectCz - stejné jako u pattern2Op_ObjectCz
-
 patternsOcrEn = {
-    "pattern1_OcrEn": pattern1G_OcrEn,
-    "pattern2_OcrEn": pattern2Op_OcrEn,
-    "pattern3_OcrEn": pattern3Ol_OcrEn,
-    "pattern4_OcrEn": pattern4Ol_OcrEn
+    "pattern1_OcrEn": functions.pattern1_OcrEn,
+    "pattern2_OcrEn": functions.pattern2_OcrEn,
+    "pattern3_OcrEn": functions.pattern3_OcrEn,
+    "pattern4_OcrEn": functions.pattern4_OcrEn
 }
 patternsOcrCz = {
-    "pattern1_OcrCz": pattern1G_OcrCz,
-    "pattern2_OcrCz": pattern2Op_OcrCz,
-    "pattern3_OcrCz": pattern3Ol_OcrCz,
-    "pattern4_OcrCz": pattern4Ol_OcrCz
+    "pattern1_OcrCz": functions.pattern1_OcrCz,
+    "pattern2_OcrCz": functions.pattern2_OcrCz,
+    "pattern3_OcrCz": functions.pattern3_OcrCz,
+    "pattern4_OcrCz": functions.pattern4_OcrCz
 }
 patternsObjectEn = {
-    "pattern1_ObjectEn": pattern1G_ObjectEn,
-    "pattern2_ObjectEn": pattern2Op_ObjectEn,
-    "pattern3_ObjectEn": pattern3Ol_ObjectEn
+    "pattern1_ObjectEn": functions.pattern1_ObjectEn,
+    "pattern2_ObjectEn": functions.pattern2_ObjectEn,
+    "pattern3_ObjectEn": functions.pattern3_ObjectEn
 }
 patternsObjectCz = {
-    "pattern1_ObjectCz": pattern1G_ObjectCz,
-    "pattern2_ObjectCz": pattern2Op_ObjectCz,
-    "pattern3_ObjectCz": pattern3Ol_ObjectCz
+    "pattern1_ObjectCz": functions.pattern1_ObjectCz,
+    "pattern2_ObjectCz": functions.pattern2_ObjectCz,
+    "pattern3_ObjectCz": functions.pattern3_ObjectCz
 }
 
 gemini_measurement = False
